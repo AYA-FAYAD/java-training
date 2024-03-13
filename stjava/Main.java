122ee522144a242a71a20affe90c208dad64657b
aya fayad <ml.aya.fayad@gmail.com>
2024-03-13 07:17:21 +0300

    dataType

--- stjava/Main.java
+++ stjava/Main.java
@@ -1,7 +1,41 @@
+import java.util.Date;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args){
-         System.out.println("Hello World, aya start learn java");
+         byte age =30;
+        //  long viewsCount = 3_123_444_676L;
+        //  float price = 10.99F;
+        //  Date now = new Date();
+
+        //  System.out.println(now);
+        
+        //  System.out.println(age);
+        // String message = new String("Hello World ");
+        // String message = "  Hello aya" + "lolo";
+        String message = "hello \"aya\"";
+        System.out.println(message);
+
+        // String message3 = "c:\\Windows\\";
+        // System.out.println(message3);
+        // String message3 = "c:\nWindows\\";
+        // System.out.println(message3);
+
+        String message3 = "c:\tWindows\\";
+        System.out.println(message3);
+        // System.out.println(message.trim());
+
+        // System.out.println(message.startsWith("H"));
+        // System.out.println(message.length());
+        // System.out.println(message.replace("lo", "*"));
+        // System.out.println(message.indexOf("e"));
+        // System.out.println(message);
+
+        // Point point1 = new Point(1, 1);
+        // Point point2 = point1;
+        // point1.x =2;
+        // System.out.println(point2);
     }
     
 }
